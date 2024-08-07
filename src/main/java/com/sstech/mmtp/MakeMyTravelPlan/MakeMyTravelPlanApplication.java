35b38de5a7bec643a287a57ad12509722dfb3d26
STrinath <trinath.967.swain@gmail.com>
2024-08-07 20:58:34 +0530

    Project added in repository

---
+++ src/main/java/com/sstech/mmtp/MakeMyTravelPlan/MakeMyTravelPlanApplication.java
@@ -0,0 +1,14 @@
+package com.sstech.mmtp.MakeMyTravelPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyTravelPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyTravelPlanApplication.class, args);
+		System.out.println("Welcome To MMTP");
+	}
+
+}
---
+++ src/test/java/com/sstech/mmtp/MakeMyTravelPlan/MakeMyTravelPlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sstech.mmtp.MakeMyTravelPlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeMyTravelPlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
